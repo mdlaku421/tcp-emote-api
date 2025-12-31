5a78b5c23437a7ebae01be6d6e185a27c17bb486
mdlaku421 <omd59194@gmail.com>
2025-12-31 23:43:22 +0600

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 import socket
+import os
 
-VPS_IP = "34.142.230.235"
-TCP_PORT = 9000
+TCP_IP = "127.0.0.1"
+TCP_PORT = 9000   # bot will listen here
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ def send_to_game(data):
     try:
         s = socket.socket()
         s.settimeout(3)
-        s.connect((VPS_IP, TCP_PORT))
+        s.connect((TCP_IP, TCP_PORT))
         s.send(data.encode())
         s.close()
         return True
@@ -30,12 +31,11 @@ def emote():
     emote = request.args.get("emote")
 
     if not region or not tc or not uid or not emote:
-        return jsonify({"error": "missing params"}), 400
+        return jsonify({"error":"missing params"}), 400
 
-    # data format: region|tc|uid|emote
     data = f"{region}|{tc}|{uid}|{emote}"
 
     if send_to_game(data):
-        return jsonify({"status": "sent", "data": data})
+        return jsonify({"status":"sent","data":data})
     else:
-        return jsonify({"status": "tcp_offline"}), 500
+        return jsonify({"status":"tcp_offline"}), 500
